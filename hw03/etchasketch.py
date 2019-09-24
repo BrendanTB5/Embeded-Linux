0a8a1f484744acb4cb9f9749eeea15d01cd25128
Brendan Mulholland <bmulholland98@gmail.com>
2019-09-24 11:47:59 -0400

    Finsihed HW3

--- hw03/etchasketch.py
+++ hw03/etchasketch.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 """
 Made by Brendan Mulholland
-ECE 434- HW #2
+ECE 434- HW #3
 """
 
-import Adafruit_BBIO.GPIO as GPIO
 from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2, eQEP1
 import time
 import smbus
-from subprocess import call
 
 
 
 
 def clearBoard():
-    global red
-    global green
     global board
-    
-    green = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
-    #red = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+    global playerX
+    global playerY
     
     board = [ [0] * 8 for _ in range(8)]
+    board[playerX][playerY] = 1
     
     displayBoard()
     
@@ -29,8 +25,8 @@ def clearBoard():
 def displayBoard():
     
     global VertMatrix
-    global green
     global red
+    global matrix
     
     PosMatrix = [0x00,0x02,0x04,0x06,0x08,0x0A,0x0C,0x0E]
     
@@ -42,107 +38,70 @@ def displayBoard():
     
     for i in range(8):
         bus.write_i2c_block_data(matrix,PosMatrix[i],[green[i],red[i]])
-    
+
+
+            
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)    
 
 
 def main():
     
-   
-    
     global playerX
     global playerY
     global board
-    
-   
-    
-    
-
     global encoder1
     global encoder2
-        
+    global red
+    global VertMatrix
+    
+    vertPos = 0
+    horiPos = 0
    
- 
-    """
-    
     while True:
-        #The prevous locations
-        board[playerY][playerX] = 'X'
-    
-        #This goes through the commands to apply the proper outcome to the commands
-        com = input("Please Enter A Command: ")
-        if com == "up":
-            playerY = playerY - 1
-        elif com == "down":
-            playerY = playerY + 1
-        elif com == "left":
-            playerX = playerX - 1
-        elif com == "right":
-            playerX += 1
-        elif com == "clear":
-            board = [['-'] * m for i in range(n)]
-     """       
+        time.sleep(.5)
         
+        
+        oldPlayerX = playerX
+        oldPlayerY = playerY
+        
+        
+        temp = bus.read_byte_data(address, 0)
+        
+        if(temp >= 26):
+            clearBoard()
+        
+        
+        if(encoder1.position < horiPos):
+            playerX += 1
+        elif(encoder1.position > horiPos):
+            playerX += -1
+        elif(encoder2.position < vertPos):
+            playerY += 1
+        elif(encoder2.position > vertPos):
+            playerY += -1
+        
+        
+        horiPos = encoder1.position
+        vertPos = encoder2.position
+        
+        playerX = clamp(playerX,0,7)
+        playerY = clamp(playerY,0,7)
+        
+        
+        
+        
+        red[oldPlayerX] = red[oldPlayerX] - VertMatrix[oldPlayerY]
+        red[playerX] = red[playerX] + VertMatrix[playerY]
+        
+        if(oldPlayerX != playerX or oldPlayerY != playerY):
+            board[playerX][playerY] = 1
             
-    try:
-        while True:
-            time.sleep(.5)
-            global horiPos
-            global vertPos
-            global red
-            global green
-            global VertMatrix
+        
+        displayBoard()
             
-            oldPlayerX = playerX
-            oldPlayerY = playerY
-            
-            
-            temp = bus.read_byte_data(address, 0)
-            
-            if(temp >= 26):
-                clearBoard()
-            
-            
-            
-            
-            
-            
-            #clearBoard("P9_10")
-            
-            
-            if(encoder1.position < horiPos):
-                playerX += 1
-            elif(encoder1.position > horiPos):
-                playerX += -1
-            elif(encoder2.position < vertPos):
-                playerY += 1
-            elif(encoder2.position > vertPos):
-                playerY += -1
-            
-            
-            horiPos = encoder1.position
-            vertPos = encoder2.position
-            
-            playerX = clamp(playerX,0,7)
-            playerY = clamp(playerY,0,7)
-            
-            
-            
-            
-            red[oldPlayerX] = red[oldPlayerX] - VertMatrix[oldPlayerY]
-            red[playerX] = red[playerX] + VertMatrix[playerY]
-            
-            if(oldPlayerX != playerX or oldPlayerY != playerY):
-                board[playerX][playerY] = 1
-                
-            
-            displayBoard()
-            
-    except KeyboardInterrupt:
-        GPIO.cleanup()
     
-            
-def clamp(n, minn, maxn):
-    return max(min(maxn, n), minn)
+
     
 
 
@@ -151,14 +110,11 @@ if __name__ == "__main__":
      #This is the size of the board and the init player location
      
     
-    #call("./configPin.sh")
-    
 
     playerX = 4
     playerY = 4
     
-    vertPos = 0
-    horiPos = 0
+
     
     bus = smbus.SMBus(2)
     address = 0x48
@@ -169,17 +125,26 @@ if __name__ == "__main__":
     bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
     
     
-    greenboard =  [ [0] * 8 for _ in range(8)]
+    
+   
     
     VertMatrix = [0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80]
     
-    green = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+    
+    board = 0
+    
+    #green = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
     red = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
+    
+    
+    #greenboard =  [ [0] * 8 for _ in range(8)]
     
 
     
      #This initialized the board to null and then adds the player's location
     clearBoard()
+    
+    #This sets up the encoders to zero
     encoder1 = RotaryEncoder(eQEP1)
     encoder2 = RotaryEncoder(eQEP2)
     encoder1.setAbsolute()
@@ -188,5 +153,6 @@ if __name__ == "__main__":
     encoder2.enable()
     
     red[playerX] = red[playerX] + VertMatrix[playerY]
+    board[playerX][playerY] = 1
     
     main()
