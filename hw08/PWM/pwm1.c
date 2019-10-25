f69f80c193d229a013ea5502750e564081cab25a
Brendan Mulholland <bmulholland98@gmail.com>
2019-10-25 12:32:31 -0400

    Finished HW8 NO README

--- hw08/PWM/pwm1.c
+++ hw08/PWM/pwm1.c
@@ -10,16 +10,16 @@ volatile register uint32_t __R31;
 
 void main(void)
 {
-	uint32_t gpio = P9_14;	// Select which pin to toggle.;
+	uint32_t gpio = P9_31;	// Select which pin to toggle.;
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
 	while (1) {
-		__R30 |= gpio;		// Set the GPIO pin to 1
-		__delay_cycles(100000000);
-		__R30 &= ~gpio;		// Clearn the GPIO pin
-		__delay_cycles(100000000);
+		__R30 |= gpio;      // Set the GPIO pin to 1
+        __delay_cycles(1);  // Delay one cycle to correct for loop time
+        __R30 &= ~gpio;     // Clear the GPIO pin
+        __delay_cycles(0);
 	}
 }
 
