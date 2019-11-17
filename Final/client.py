9cf67334411d7ddc28bb2337b0086a93d9919588
Brendan Mulholland <mulholbn@rose-hulman.edu>
2019-11-17 15:39:33 -0500

    Chat demo

--- Final/client.py
+++ Final/client.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
+# Python program to implement client side of chat room. 
+import socket 
+import select 
+import sys 
 
-import socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+if len(sys.argv) != 3: 
+	print("Correct usage: script, IP address, port number")
+	exit() 
+IP_address = str(sys.argv[1]) 
+Port = int(sys.argv[2]) 
+server.connect((IP_address, Port)) 
 
-HOST = '10.0.2.15'
-PORT = 8080
+while True: 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+	# maintains a list of possible input streams 
+	sockets_list = [sys.stdin, server] 
 
-client.send("Hello".encode())
+	""" There are two possible input situations. Either the 
+	user wants to give manual input to send to other people, 
+	or the server is sending a message to be printed on the 
+	screen. Select returns from sockets_list, the stream that 
+	is reader for input. So for example, if the server wants 
+	to send a message, then the if condition will hold true 
+	below.If the user wants to send a message, the else 
+	condition will evaluate as true"""
+	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
 
-from_server = client.recv(4096)
-
-client.close()
-
-print (from_server)
+	for socks in read_sockets: 
+		if socks == server: 
+			message = socks.recv(2048)
+			print(message) 
+		else: 
+			message = sys.stdin.readline() 
+			server.send(message.encode()) 
+			sys.stdout.write("<You>") 
+			sys.stdout.write(message) 
+			sys.stdout.flush() 
+server.close() 
